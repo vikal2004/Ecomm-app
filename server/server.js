c1e159ce9920ccc233258e1c8e6e0acbf7c13c2a
vikal singh <114324236+vikal2004@users.noreply.github.com>
2025-07-24 22:40:38 +0530

    added-file

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://ecomm-app-weld.vercel.app",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
